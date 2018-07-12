0775829c0101b54b6e62ba46bb9fa23877054761
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-12 05:40:30 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
   i = 0 
   while i < collection.length
     if yield(collection[i])
-    new_array << yield(collection[i])
+    new_array << (collection[i])
     i += 1 
   end 
   

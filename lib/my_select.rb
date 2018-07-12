6c0fbd577165cbab98399bb5a2629fb3ccb18fc4
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-12 05:41:45 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+  new_array = []
+  i = 0 
+  while i < collection.length
+    if yield(collection[i])
+    new_array << (collection[i])
+    end
+    i += 1 
+  end 
+  new_array
 end
+
+# { |i| puts i * 2 }
+# i is the yielded value and 
+# puts i * 2 is what we want to do
+#   with the yielded value!
